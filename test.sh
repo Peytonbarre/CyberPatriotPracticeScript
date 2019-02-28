442755afb9cb283f256ca69a3f7e25db0dc2b776
Peytonbarre <44472217+Peytonbarre@users.noreply.github.com>
2019-02-28 17:00:53 -0600

    Add files via upload

--- test.sh
+++ test.sh
@@ -11,15 +11,43 @@ startgey()
 	clear
 
 	clamgey
+	updategey
+	passagegey
 
 	printf "\033[1;31mDone!\033[0m\n"
 }
 
+
+#Updates Linux using apt-get
+updategey(){
+
+	sudo apt-get upgrade
+	sudo apt-get clean
+
+	cont
+}
+
+
+
+
+#Installs and run all of the ClamAv functions
 clamgey(){
 	
 	printf "Downloading ClamAv..."
 	sudo apt-get install clamav
 
+	cont
+}
+
+
+#Changes the password min/max age
+passagegey(){
+
+	sed 's/99999/90/g' /etc/login.defs
+
+	
+
+	cont
 }
 
 startgey
